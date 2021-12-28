762b044d31ffc69e6f1480b67c7734be971feb1a
Codrut Panea <codrut.panea@esolutions.ro>
2021-12-28 13:56:05 +0200

    test

--- src/main/java/bwlab/borg1/borgwarner/BorgwarnerApplication.java
+++ src/main/java/bwlab/borg1/borgwarner/BorgwarnerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BorgwarnerApplication {
 
-    public static void main(String[] args) {
         SpringApplication.run(BorgwarnerApplication.class, args);
     }
 
