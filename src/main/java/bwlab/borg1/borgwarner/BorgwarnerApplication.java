831120726bc5fd21342ebf2fb12e60e1e6d973ed
Codrut Panea <codrut.panea@esolutions.ro>
2022-02-08 11:35:07 +0200

    add tests for default controller

--- src/main/java/bwlab/borg1/borgwarner/BorgwarnerApplication.java
+++ src/main/java/bwlab/borg1/borgwarner/BorgwarnerApplication.java
@@ -2,21 +2,11 @@ package bwlab.borg1.borgwarner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class BorgwarnerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BorgwarnerApplication.class, args);
-//        dwdw
     }
-
-    @GetMapping
-    public String helloWorld() {
-        return "revision 1 ";
-    }
-
 }
--- src/test/java/bwlab/borg1/borgwarner/BorgwarnerApplicationTests.java
+++ src/test/java/bwlab/borg1/borgwarner/BorgwarnerApplicationTests.java
@@ -1,13 +1,39 @@
 package bwlab.borg1.borgwarner;
 
+import bwlab.borg1.borgwarner.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BorgwarnerApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private HomeController homeController;
 
     @Test
     void contextLoads() {
+        assertThat(homeController).isNotNull();
     }
 
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, World")));
+    }
 }
